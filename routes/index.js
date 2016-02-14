1a5befcff808f10630d750b5ebaa6eb3998fad52
SruthiKuppa <sruthi1020s@gmail.com>
2016-02-14 09:00:01 -0500

    added contact,services,projects.ejs files

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express on Azure' });
+  res.render('index', { title: 'My home Page' });
 });
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('aboutme', { title: 'About Me' });
+});
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'projects' });
+});
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'services' });
 });
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'contact Me' });
-});
-/* GET projects page. */
-router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'projects' });
 });
 module.exports = router;
