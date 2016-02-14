acfc5960e167d18f93a1c1d38285c194d778453a
SruthiKuppa <sruthi1020s@gmail.com>
2016-02-14 10:17:57 -0500

    added some formatting to ejs files

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My home Page' });
+  res.render('index', { title: 'MY HOME PAGE' });
 });
 /* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.render('aboutme', { title: 'About Me' });
+router.get('/aboutme', function(req, res, next) {
+  res.render('aboutme', { title: 'ABOUT ME' });
 });
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'projects' });
+  res.render('projects', { title: 'PROJECTS' });
 });
 /* GET services page. */
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'services' });
+  res.render('services', { title: 'SERVICES' });
 });
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'contact Me' });
+  res.render('contact', { title: 'CONTACT ME' });
 });
 module.exports = router;
